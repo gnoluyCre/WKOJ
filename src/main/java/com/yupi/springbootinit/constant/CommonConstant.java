43adbf602885b9ca59e7eef538098f221f07bce8
Jea152 <1815041011@qq.com>
2024-06-21 13:54:26 +0800

    项目初始化模板

---
+++ src/main/java/com/yupi/springbootinit/constant/CommonConstant.java
@@ -0,0 +1,21 @@
+package com.yupi.springbootinit.constant;
+
+/**
+ * 通用常量
+ *
+ * @author <a href="https://github.com/gnoluyCre/WKOJ"></a>
+ * @from 
+ */
+public interface CommonConstant {
+
+    /**
+     * 升序
+     */
+    String SORT_ORDER_ASC = "ascend";
+
+    /**
+     * 降序
+     */
+    String SORT_ORDER_DESC = " descend";
+    
+}
